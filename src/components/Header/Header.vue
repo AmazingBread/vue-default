a26488878351c65e0e6d5ba33f604a5f9281b22d
bread_peaty <83941202+AmazingBread@users.noreply.github.com>
2025-02-18 09:50:25 +0900

    기본 코딩 적용

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="header">
-    <img src="@/assets/image/kaia-logo.svg" class="kaia-logo" />
-    <h1>IoT 융합 <em>해안도시 침수대응</em><br />기술 개발 및 실증연구</h1>
+   header
   </div>
 </template>
 
